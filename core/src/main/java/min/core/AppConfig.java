175c6c4198bcfa56bcdab38ab42a1e722d70e79c
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-29 17:21:53 +0900

    리팩토링

--- core/src/main/java/min/core/AppConfig.java
+++ core/src/main/java/min/core/AppConfig.java
@@ -1,7 +1,9 @@
 package min.core;
 
+import min.core.discount.Discount;
 import min.core.discount.FixDiscount;
 import min.core.member.MemMemberRepository;
+import min.core.member.MemberRepository;
 import min.core.member.MemberService;
 import min.core.member.MemberServiceImpl;
 import min.order.OrderService;
@@ -9,13 +11,20 @@ import min.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemMemberRepository(),
-                new FixDiscount(
-                ));
+                memberRepository(),
+                discount());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemMemberRepository();
+    }
+
+    public Discount discount() {
+        return new FixDiscount();
     }
 }
