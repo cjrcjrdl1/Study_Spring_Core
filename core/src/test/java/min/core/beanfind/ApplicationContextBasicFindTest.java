742541f956495a428ef3b6efe9966e8b7bca47da
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-30 15:17:24 +0900

    기본적인 스프링 빈 조회

---
+++ core/src/test/java/min/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,38 @@
+package min.core.beanfind;
+
+import min.core.AppConfig;
+import min.core.member.MemberService;
+import min.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈이름으로 조회x")
+    void findBeanByNoName() {
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class, () ->
+                ac.getBean("xxxx", MemberService.class));
+    }
+}
