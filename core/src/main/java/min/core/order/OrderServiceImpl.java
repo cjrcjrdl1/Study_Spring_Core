13970adb33364a4e374383df9a46b7a38ca85ce1
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-21 11:04:45 +0900

    롬복 활용

--- core/src/main/java/min/core/order/OrderServiceImpl.java
+++ core/src/main/java/min/core/order/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package min.core.order;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import min.core.discount.Discount;
 import min.core.member.Member;
 import min.core.member.MemberRepository;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -17,11 +20,22 @@ public class OrderServiceImpl implements OrderService {
         return memberRepository;
     }
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, Discount discount) {
-        this.memberRepository = memberRepository;
-        this.discount = discount;
-    }
+
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, Discount discount) {
+//        this.memberRepository = memberRepository;
+//        this.discount = discount;
+//    }
+
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscount(Discount discount) {
+//        this.discount = discount;
+//    }
 
     @Override
     public Order makeOrder(Long memberId, String itemName, int itemPrice) {
