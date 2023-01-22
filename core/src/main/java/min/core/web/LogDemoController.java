a0a52e86a7594b30004c60e07c01b487bb628bce
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-22 21:49:39 +0900

    request 스코프 예제

---
+++ core/src/main/java/min/core/web/LogDemoController.java
@@ -0,0 +1,27 @@
+package min.core.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import min.core.common.MyLogger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
