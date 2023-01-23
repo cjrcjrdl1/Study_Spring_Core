cd4ef031fca45fc667abcdbb3d79832e1923128b
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-23 09:56:24 +0900

    스코프와 Provider

--- core/src/main/java/min/core/web/LogDemoService.java
+++ core/src/main/java/min/core/web/LogDemoService.java
@@ -2,13 +2,15 @@ package min.core.web;
 
 import lombok.RequiredArgsConstructor;
 import min.core.common.MyLogger;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
